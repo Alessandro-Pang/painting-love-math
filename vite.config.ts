80303b8a687844b27cad19de5ac1d638834bc10f
Alessandro-pang <pcy1314@163.com>
2024-11-05 13:24:27 +0800

    chore: 修改 github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig((command) => {
+  const base = command.mode === 'production' ? '/painting-love-math/' : '/'
+  return {
+    base,
+    plugins: [vue()],
+  }
 })
